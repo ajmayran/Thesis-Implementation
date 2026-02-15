0ae9f61887914256bf7d29f99df85407ae136952
Alvan Jay Mayran <clickerz09@gmail.com>
2026-02-15 11:11:22 +0800

    update

--- thesis_project/thesis_project/settings.py
+++ thesis_project/thesis_project/settings.py
@@ -40,6 +40,8 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -149,7 +151,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
     "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
     "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
