e3cadbb210a2cedd83e203d0997920c94eeb1d09
Alvan Jay Mayran <clickerz09@gmail.com>
2026-02-17 11:38:39 +0800

    update

--- thesis_project/thesis_project/settings.py
+++ thesis_project/thesis_project/settings.py
@@ -24,9 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '))"                                                                             q=cw)h4)(4*uvx#(^+j9*i9k2e0u5(5747@+3mq5xjdk9-3f44')
 
-CSRF_TRUSTED_ORIGINS = [
-    f"https://{os.environ['WEBSITE_HOSTNAME']}"
-]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
